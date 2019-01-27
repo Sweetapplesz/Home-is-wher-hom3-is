db9edf7b25023cf064cec1f2098a124e1a681d63
Ryan <46606201+Ryancomix@users.noreply.github.com>
2019-01-27 15:02:35 -0500

    recommit

---
+++ Megalo 4.21/Intermediate/Source/Megalo.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Megalo : ModuleRules
+{
+	public Megalo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
